6535017a226b19e9ae7a6aa008097ced2728332a
Jaspers W <hwilliamnq@gmail.com>
2015-07-29 20:02:49 -0500

    download ready!

--- gscritps/download.py
+++ gscritps/download.py
@@ -10,7 +10,7 @@ url = "http://financials.morningstar.com/ajax/ReportProcess4CSV.html?&t={}" \
       "&region=usa&culture=en-US&cur=USD&reportType={}&period={}&dataType=R" \
       "&order=asc&columnYear=5&rounding=3&view=raw&denominatorView=raw&number=2"
 
-stock = ['CRPJF','TKECY','KAEPY','SSEZF','CPYYF','IBDSF','TERRF','ELCPF','EDPFY','ESOCF',
+stock = ['CRPJF', 'TKECY', 'KAEPY', 'SSEZF', 'CPYYF', 'IBDSF', 'TERRF', 'ELCPF', 'EDPFY', 'ESOCF',
  'CPYYY','ELPSF','STRNY','IBDRY','SVTRF','MAEOY','TEZNY','GASNF','ENLAY','SZEVY',
  'NGGTF','ELEZF','ABZPF','ECIFF','RWNEF','SZEVF','SSEZY','AGLNF','HOKCF','ED',
  'RDEIF','NEE','NGG','HOKCY','HPIFF','DCUC','UUGWF','SCG','PPL','SO','CLPHY',
@@ -25,27 +25,26 @@ period = 12  # In months, 12 = annual, 3 = quarterly.
 
 path = '/home/williamn/Repository/data/financialData/foreignCompanies/sector/utilities'
 
-for i in stock:
-    for j in report_type:
-        request = urllib2.urlopen(url.format(stock[i], report_type[j], period))
+for stocki in stock:
+    for reptypej in report_type:
+        request = urllib2.urlopen(url.format(stocki, reptypej, period))
 
-        fileinfo = request.info()
+        fileInfo = request.info()
         data = request.read()
 
         f = StringIO.StringIO(data)
         reader = csv.reader(f)
 
-        filename = stock + '_' + reptype + '.csv'
+        filename = stocki + '_' + reptypej + '.csv'
         if os.path.exists(path) == 0:
             os.makedirs(path)
-        newfile = open(os.path.join(path, filename), 'w')
-        filewriter = csv.writer(newfile)
+        newFile = open(os.path.join(path, filename), 'w')
+        fileWriter = csv.writer(newFile)
 
         for row in reader:
             if len(row) != 1:
-                filewriter.writerow(row)
-                #print row
+                fileWriter.writerow(row)
+                # print row
+        newFile.close()
 
-        print i + ' done!'
-
-        newfile.close()
+    print stocki + ' done!'
