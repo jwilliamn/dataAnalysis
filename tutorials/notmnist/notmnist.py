94295fa876c13d0c05333c32a6f1f2f3a19c2921
J. Williamn <hwilliamnq@gmail.com>
2017-02-20 14:44:59 +0000

    nomnist tutorial beggins

---
+++ tutorials/notmnist/notmnist.py
@@ -0,0 +1,73 @@
+""" notMNIST assignment 1
+	from Udacity
+	Deep Learning course"""
+
+from __future__ import print_function
+import matplotlib.pyplot as plt 
+import numpy as np 
+import os 
+import sys
+import tarfile
+
+from scipy import ndimage
+from sklearn.linear_model import LogisticRegression
+from six.moves.urllib.request import urlretrieve
+from six.moves import cPickle as pickle
+
+url = 'http://commondatastorage.googleapis.com/books1000/'
+last_percent_reported = None
+data_root = '/home/williamn/Repository/data/nomnist'
+
+def download_progress_hook(count, blockSize, totalSize):
+	"""A hook to report the progress of a download. This is mostly intended for users with
+  	slow internet connections. Reports every 5% change in download progress.
+  	"""
+	global last_percent_reported
+	percent = int(count * blockSize * 100 / totalSize)
+
+	if last_percent_reported != percent:
+		if percent % 5 == 0:
+			sys.stdout.write("%s%%" % percent)
+			sys.stdout.flush()
+		else:
+			sys.stdout.write(".")
+			sys.stdout.flush()
+
+		last_percent_reported = percent
+
+
+def maybe_download(filename, expected_bytes, force=False):
+	""" Download a file if not present, and make sure it's the right size."""
+	dest_filename = os.path.join(data_root, filename)
+	if force or not os.path.exists(dest_filename):
+		print('Attempting to download:', filename)
+		filename, _ = urlretrieve(url+filename, dest_filename, reporthook=download_progress_hook)
+		print('\nDownload Complete!')
+	statinfo = os.stat(dest_filename)
+	if statinfo.st_size == expected_bytes:
+		print('Found and verified', dest_filename)
+	else:
+		raise Exception(
+			'Failed to verify' + dest_filename + '. Can you get to it with a browser?')
+	return dest_filename
+
+
+train_filename = maybe_download('notMNIST_large.tar.gz', 247336696)
+test_filename = maybe_download('notMNIST_small.tar.gz', 8458043)
+
+
+num_classes = 10
+np.random.seed(133)
+
+def maybe_extract(filename, force=False):
+	root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .tar.gz
+	if os.path.isdir(root) and not force:
+		# You may override by setting force=True.
+		print('%s already prenset - Skipping extraction of %s.' % (root, filename))
+	else:
+		print('Extracting data for %s. This may take a while. please wait.' % root)
+		tar = tarfile.open(filename)
+		sys.stdout.flush()
+		tar.extractall(data_root)
+		tar.close()
+	data_folders = 
--- tutorials/python/pythonTutorial.py
+++ tutorials/python/pythonTutorial.py
@@ -2,3 +2,9 @@
 # Basic data types
 x = 3
 print(x,type(x))
+
+import numpy as np
+
+a = np.array([1,2,3])
+
+print(a)
