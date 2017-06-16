f45fd2eabe803d357497d969b737c4d14a922a3e
J. Williamn <hwilliamnq@gmail.com>
2017-06-16 09:57:51 -0500

    catch exception added

--- gscritps/pdfRetrieve.py
+++ gscritps/pdfRetrieve.py
@@ -2,6 +2,7 @@ __author__ = 'williamn'
 # Python script to download financial information from NYSE - financials.morningstar.com webpage
 
 import os
+from urllib.error import URLError
 from urllib.request import urlopen
 
 
@@ -14,11 +15,14 @@ url = ["http://tramite.midis.gob.pe/institucional/aplicativos/oad/sitradocV2/arc
 def download_file(download_url):
     pdfname = os.path.basename(download_url)
     
-    response = urlopen(download_url)
-    file = open(pdfname, 'wb')
-    file.write(response.read())
-    file.close()
-    print("pdfname: ", pdfname, "Completed")
+    try:
+        response = urlopen(download_url)
+        file = open(pdfname, 'wb')
+        file.write(response.read())
+        file.close()
+        print("pdfname: ", pdfname, "Completed")
+    except URLError:
+        print("Bad url")
 
 def main():
     #download_file("http://tramite.midis.gob.pe/institucional/aplicativos/oad/sitradocV2/archivos/450254.pdf")
