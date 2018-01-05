2fa31911357e003b0dceae9127ebf43eb56e6e5a
J. Williamn <hwilliamnq@gmail.com>
2018-01-05 17:12:51 -0500

    exploratory

--- examples/attrition/attritionBbvaXGBt.py
+++ examples/attrition/attritionBbvaXGBt.py
@@ -25,6 +25,7 @@ import pandas as pd
 import xgboost as xgb
 import matplotlib.pyplot as plt
 
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
 from exploratory import X_train, X_test, y_train, y_test, Xtest, train_
 
 
@@ -50,17 +51,45 @@ def compute_cost(AL, Y):
 
 X_train, X_test, Xtest = X_train.T, X_test.T, Xtest.T
 y_train, y_test = np.ravel(y_train.T), np.ravel(y_test.T)
+#plt.matshow(pd.DataFrame(X_train).corr())
+
 
 # Instantiate the XGBClassifier: xg_cl
-xg_cl = xgb.XGBClassifier(learning_rate=0.06, max_depth=6, objective='binary:logistic', n_estimators=10000, seed=123)
+xg_cl = xgb.XGBClassifier(learning_rate=0.09, max_depth=6, objective='binary:logistic', n_estimators=1200, seed=123)
+#xg_cl = xgb.XGBClassifier()
+
+
+## Grid search - hyperparameter tunning
+#learning_rate = [0.0001, 0.001, 0.01, 0.1, 0.2, 0.3]
+#param_grid = dict(learning_rate=learning_rate)
+#
+#n_estimators = [50, 100, 150, 200, 1000]
+#max_depth = [2, 4, 6, 8]
+#param_grid = dict(max_depth=max_depth, n_estimators=n_estimators)
+#
+#kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=7)
+#grid_search = GridSearchCV(xg_cl, param_grid, scoring="neg_log_loss", n_jobs=-1, cv=kfold, verbose=1)
+#grid_result = grid_search.fit(X_train, y_train)
+## summarize results
+#print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+#means = grid_result.cv_results_['mean_test_score']
+#stds = grid_result.cv_results_['std_test_score']
+#params = grid_result.cv_results_['params']
+#for mean, stdev, param in zip(means, stds, params):
+#	print("%f (%f) with: %r" % (mean, stdev, param))
+
 
 # Fit the classifier to the training set
 eval_set = [(X_test, y_test)]
-xg_cl.fit(X_train, y_train, early_stopping_rounds=30, eval_metric="logloss", eval_set=eval_set, verbose=True)
+xg_cl.fit(X_train, y_train, early_stopping_rounds=40, eval_metric="logloss", eval_set=eval_set, verbose=True)
 
 print(xg_cl.feature_importances_)
 xgb.plot_importance(xg_cl)
 plt.show()
+
+xgb.plot_tree(xg_cl)
+plt.show()
+
 
 # cost of training
 preds_train = xg_cl.predict(X_train)
